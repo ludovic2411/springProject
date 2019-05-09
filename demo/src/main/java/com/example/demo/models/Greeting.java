57dfb5530ecf9fd23d10a0cf6469d7ea284560b4
ludovic2411 <ludovic.ceccotti@gmail.com>
2019-05-09 09:30:19 +0200

    first

---
+++ demo/src/main/java/com/example/demo/models/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+public class Greeting {
+	private String message;
+	
+	public Greeting(String m) {
+		this.message=m;
+	}
+	public String getMessage() {
+		return this.message;
+	}
+}
